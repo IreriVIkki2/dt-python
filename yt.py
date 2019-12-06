e60d13c4ebc0e716dde4cc71123ebb9fe857f37a
IreriVikki <wambsviki@gmail.com>
2019-12-06 15:41:31 +0300

    added api changing system

--- yt.py
+++ yt.py
@@ -47,7 +47,7 @@ def get_video(_max_video_length):
         return res_json["video"]
 
     elif action == 205:
-        time.sleep(5)
+        time.sleep(15)
         create_queue()
         return get_video(_max_video_length)
 
